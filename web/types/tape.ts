2cdcdd1c9cbacdad618bc1a3a7f28563f0435e4f
simfonik <simfonik@gmail.com>
2026-01-07 15:21:26 -0800

    Add tape data types

---
+++ web/types/tape.ts
@@ -0,0 +1,37 @@
+export type DJ = {
+    name: string;
+    slug: string;
+  };
+  
+  export type TapeImages = {
+    cover?: string;
+  };
+  
+  export type AudioLink = {
+    label: string;
+    url: string;
+  };
+  
+  export type Track = {
+    artist: string;
+    title: string;
+    duration?: string;
+  };
+  
+  export type Side = {
+    position: string;
+    title?: string;
+    audio_links: AudioLink[];
+    image?: string;
+    tracks?: Track[];
+  };
+  
+  export type Tape = {
+    id: string;
+    title: string;
+    released: string; // recommend YYYY-MM-DD in sample data for consistent sorting
+    djs: DJ[];
+    images?: TapeImages;
+    sides: Side[];
+  };
+  
